0d0c675f7abfc4b53cea06be5e9cfebf3c2bfc50
GenyaElamkov <genyaelamkov@gmail.com>
2024-03-04 19:11:58 +0300

    lesson 5.9 close

--- Module_5/lesson 5.9_step_25.py
+++ Module_5/lesson 5.9_step_25.py
@@ -1,2 +1,58 @@
-def limited_hash(left, right, hash_function):
-    pass
+def limited_hash(left, right, hash_function=hash):
+    def wrapper(args):
+        result = hash_function(args)
+        arr = list(range(left, right + 1))
+        if result in arr:
+            return result
+
+        while True:
+            if result > right:
+                return arr[(result - right - 1) % (right - left + 1)]
+            if result < left:
+                return arr[-((left - result) % (right - left + 1))]
+
+    return wrapper
+
+
+# Вариант решения.
+# def limited_hash(left, right, hash_function=hash):
+#     def inner_hash_function(x):
+#         return left + (hash_function(x) - left) % (right - left + 1)
+#     return inner_hash_function
+
+
+
+# hash_function = limited_hash(10, 15)
+
+# print(hash_function(10))
+# print(hash_function(11))
+# print(hash_function(15))
+
+
+# hash_function = limited_hash(10, 15)
+
+# print(hash_function(16))
+# print(hash_function(17))
+# print(hash_function(21))
+# print(hash_function(22))
+# print(hash_function(23))
+
+
+# hash_function = limited_hash(10, 15)
+
+# print(hash_function(9))
+# print(hash_function(8))
+# print(hash_function(4))
+# print(hash_function(3))
+# print(hash_function(2))
+
+
+hash_function = limited_hash(2, 3, hash_function=lambda obj: len(str(obj)))
+
+print(hash_function('a'))
+print(hash_function('ab'))
+print(hash_function('abc'))
+print(hash_function('abcd'))
+print(hash_function('abcde'))
+print(hash_function('abcdef'))
+print(hash_function('abcdefg'))
