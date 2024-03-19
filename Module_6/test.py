7e7be99ce274412efd0044157be7d8c5974c5f12
GenyaElamkov <genyaelamkov@gmail.com>
2024-03-19 11:34:53 +0300

    lesson 6.2 step 19 close

--- Module_6/test.py
+++ Module_6/test.py
@@ -1,3 +1,8 @@
-s = 'afaff'
-print(type(s))
-print(str(s))
+from collections import OrderedDict
+
+# grouper = Grouper(["bee", "geek", "one", "two", "five", "hi"], key=len
+
+
+arr = ["bee", "geek", "one", "two", "five", "hi"]
+order = OrderedDict(arr)
+print(order)
