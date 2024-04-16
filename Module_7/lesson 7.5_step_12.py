9e6b77c2ef2651c012846360c5dd78c2d5066617
GenyaElamkov <genyaelamkov@gmail.com>
2024-04-16 12:31:35 +0300

    lesson 7.5 step 12 close

--- Module_7/lesson 7.5_step_12.py
+++ Module_7/lesson 7.5_step_12.py
@@ -3,8 +3,9 @@ from abc import ABC, abstractclassmethod
 
 class ChessPiece(ABC):
     def __init__(self, horizontal: str, vertical: int) -> None:
-        self.horizontal = ord(chr(horizontal) - 96)
+        self.horizontal = horizontal
         self.vertical = vertical
+        self._horizontal_num = ord(horizontal) - 96
 
     @abstractclassmethod
     def can_move(self):
@@ -13,48 +14,36 @@ class ChessPiece(ABC):
 
 class King(ChessPiece):
     def can_move(self, h: str, v: int):
-        # horiz_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
-        
-        d = {"a" :1, "b" :2, "c" :3, "d" :4, "e" :5, "f" :6,"g" :7, "h" : 8}
-        fir, sec = input(), input()
-        print(d[a[0]])
-        f1, f2, s1, s2 = d[fir[0]], int(fir[1]), d[sec[0]], int(sec[1])
-        if abs(f1 - s1) in [0, 1] and abs(f2 - s2) in [0, 1]:
-            print("YES")
-        else:
-            print("NO")
-
-        # self.horizontal = int(input())  # Column number of the first cell
-        # self.vertical = int(input())  # Row number of the first cell
-        # x1 = int(input())  # Column number of the second cell
-        # y1 = int(input())  # Row number of the second cell
-
-        # h = ord(chr(h) - 96)
-
-        # if (1 >= h-self.horizontal >= -1) and (1 >= v-self.vertical >= -1):
-        #     print('YES')
-        # else:
-        #     print('NO')
+        h = ord(h) - 96
+        if self._horizontal_num == h and self.vertical == v:
+            return False
+        if (1 >= h - self._horizontal_num >= -1) and (1 >= v - self.vertical >= -1):
+            return True
+        return False
 
 
-# class Knight(ChessPiece):
-#     def can_move(self, horizontal: str, vertical: int):
-#         possible_moves = [
-#             [horizontal + 2, vertical + 1], [horizontal + 2, vertical - 1],
-#             [horizontal - 2, vertical + 1], [horizontal - 2, vertical - 1],
-#             [horizontal + 1, vertical + 2], [horizontal + 1, vertical - 2],
-#             [horizontal - 1, vertical + 2], [horizontal - 1, vertical - 2]
-#         ]
+class Knight(ChessPiece):
+    def can_move(self, h: str, v: int):
+        h = ord(h) - 96
+        if (
+            (h - self._horizontal_num) * (h - self._horizontal_num)
+            + (v - self.vertical) * (v - self.vertical)
+            == 5
+            and 0 <= h <= 8
+            and 0 <= v <= 8
+        ):
+            return True
+        return False
 
 
 # INPUT DATA:
 
-# TEST_1:
-king = King('b', 2)
+# # TEST_1:
+# king = King('b', 2)
 
-print(king.can_move('c', 3))
-print(king.can_move('a', 1))
-print(king.can_move('f', 7))
+# print(king.can_move('c', 3))
+# print(king.can_move('a', 1))
+# print(king.can_move('f', 7))
 
 # # TEST_2:
 # knight = Knight('c', 3)
@@ -63,11 +52,11 @@ print(king.can_move('f', 7))
 # print(knight.can_move('e', 4))
 
 # # TEST_3:
-# king = King('e', 3)
+# king = King("e", 3)
 
-# print(king.can_move('e', 3))
-# print(king.can_move('e', 4))
-# print(king.can_move('b', 1))
+# print(king.can_move("e", 3))
+# print(king.can_move("e", 4))
+# print(king.can_move("b", 1))
 
 # # TEST_4:
 # knight = Knight('h', 8)
@@ -91,24 +80,30 @@ print(king.can_move('f', 7))
 #     for vertical in range(1, 9):
 #         print(f'{horizontal}{vertical}', king.can_move(horizontal, vertical))
 
-# # TEST_7:
-# kings = [King(h, v) for h in 'abcdefgh' for v in range(1, 9)]
+# TEST_7:
+kings = [King(h, v) for h in "abcdefgh" for v in range(1, 9)]
 
-# for king in kings:
-#     print('*' * 20)
-#     for horizontal in 'abcdefgh':
-#         for vertical in range(1, 9):
-#             if king.can_move(horizontal, vertical):
-#                 print(f'King({king.horizontal}{king.vertical})', f'{horizontal}{vertical}',
-#                       king.can_move(horizontal, vertical))
+for king in kings:
+    print("*" * 20)
+    for horizontal in "abcdefgh":
+        for vertical in range(1, 9):
+            if king.can_move(horizontal, vertical):
+                print(
+                    f"King({king.horizontal}{king.vertical})",
+                    f"{horizontal}{vertical}",
+                    king.can_move(horizontal, vertical),
+                )
 
 # # TEST_8:
-# knights = [Knight(h, v) for h in 'abcdefgh' for v in range(1, 9)]
+# knights = [Knight(h, v) for h in "abcdefgh" for v in range(1, 9)]
 
 # for knight in knights:
-#     print('*' * 20)
-#     for horizontal in 'abcdefgh':
+#     print("*" * 20)
+#     for horizontal in "abcdefgh":
 #         for vertical in range(1, 9):
 #             if knight.can_move(horizontal, vertical):
-#                 print(f'Knight({knight.horizontal}{knight.vertical})', f'{horizontal}{vertical}',
-#                       knight.can_move(horizontal, vertical))
+#                 print(
+#                     f"Knight({knight.horizontal}{knight.vertical})",
+#                     f"{horizontal}{vertical}",
+#                     knight.can_move(horizontal, vertical),
+#                 )
