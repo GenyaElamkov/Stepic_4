3a76ec6f87cbac03953ff920e5063f3bb2b580e2
GenyaElamkov <genyaelamkov@gmail.com>
2024-04-23 14:18:48 +0300

    lesson 8.5 close

--- Module_8/test.py
+++ Module_8/test.py
@@ -1,21 +1,13 @@
-import functools
+from dataclasses import dataclass, field
 
-def add_attr_to_instances(cls):
-    old_init = cls.__init__             # сохраняем исходный инициализатор
-    
-    @functools.wraps(old_init)
-    def new_init(self, *args, **kwargs):
-        old_init(self, *args, **kwargs) # вызываем исходный инициализатор
-        self.attr = None                # добавляем экземпляру класса атрибут attr
-    
-    cls.__init__ = new_init             # заменяем исходный инициализатор новым
-    return cls
-
-@add_attr_to_instances
-class MyClass:
-    pass
+@dataclass
+class Person:
+    name: str
+    surname: str
+    age: int = field(init=True)
 
 
-obj = MyClass()
+person1 = Person('Гвидо', 'ван Россум', 67)
+person2 = Person('Гвидо', 'ван Россум', 68)
 
-print(obj.attr)
+print(person1 == person2)
